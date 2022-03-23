31c6e638bbfc941b8ca024ce3601821df11619a7
filipandersson3 <filip.andersson3@elev.ga.ntig.se>
2022-03-23 13:41:55 +0100

    edit och delete lagt till

--- routes/meeps.js
+++ routes/meeps.js
@@ -41,10 +41,104 @@ router.post('/', async (req, res, next) => {
         console.log(err);
         res.status(500).json({
             videos: {
-                error: 'Error posting videos'
+                error: 'Error posting meeps'
             }
         })
     });
-})
+});
+
+/* DELETE /:id - delete a task by id */
+router.get('/:id/delete/', async (req, res, next) => {
+    const id = req.params.id;
+    if (isNaN(req.params.id)) {
+        res.status(400).json({
+            videos: {
+                error: 'Bad request'
+            }
+        })
+    } else {
+        await pool.promise()
+            .query('DELETE FROM meeps WHERE id = ?', [id])
+            .then((response) => {
+                console.log(response);
+                if (response[0].affectedRows === 1) {
+                    res.redirect('/meeps');
+                } else {
+                    res.status(400).redirect('/meeps');
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    videos: {
+                        error: 'Error getting meeps'
+                    }
+                })
+            });
+    }
+});
+
+router.get('/:id', async (req, res, next) => {
+    const id = req.params.id;
+    if (isNaN(id)) {
+        res.status(400).json({
+            videos: {
+                error: 'Bad request'
+            }
+        })
+    } else {
+        await pool.promise()
+            .query('SELECT * FROM meeps WHERE id = ?', [id])
+            .then(([rows, fields]) => {
+                console.log(rows);
+                let data = {
+                    message: "He llo",
+                    layout: 'layout.njk',
+                    title: 'Nunjucks example',
+                    items: rows,
+                  };
+                  res.render('meepedit.njk', data);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    videos: {
+                        error: 'Error getting meeps'
+                    }
+                })
+            });
+    }
+});
+
+router.post('/:id', async (req, res, next) => {
+    const id = req.params.id;
+    const body = req.body.body;
+    if (isNaN(id)) {
+        res.status(400).json({
+            videos: {
+                error: 'Bad request'
+            }
+        })
+    } else {
+        await pool.promise()
+        .query('UPDATE meeps SET body = ? WHERE id = ?', [body,id])
+        .then((response) => {
+            console.log(response);
+            if (response[0].affectedRows === 1) {
+                res.redirect('/meeps');
+            } else {
+                res.status(400).redirect('/meeps');
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                videos: {
+                    error: 'Error posting meeps'
+                }
+            })
+        });
+    }
+});
 
 module.exports = router;
